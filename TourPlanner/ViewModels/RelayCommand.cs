73378db0eb6cc09eb6c8a60fbd71612c342a8680
abdulbahir <94755165+abdulbahir@users.noreply.github.com>
2024-05-08 15:37:50 +0200

    implementing RelayCommand

---
+++ TourPlanner/ViewModels/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TourPlanner.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _executeAction;
+
+        public RelayCommand(Action<object> executeAction)
+        {
+            _executeAction = executeAction;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _executeAction(parameter);
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
--- TourPlanner/obj/Debug/net6.0-windows/TourPlanner.AssemblyInfo.cs
+++ TourPlanner/obj/Debug/net6.0-windows/TourPlanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1440028fc49695111e9147e887d254fe6558952c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+344fd3c9d19a420d1a69442bfe5045db56928c94")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
