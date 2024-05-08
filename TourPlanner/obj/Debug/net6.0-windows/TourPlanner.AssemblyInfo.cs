10645f661bb68bb6fb870bb408a5584c0386f750
abdulbahir <94755165+abdulbahir@users.noreply.github.com>
2024-05-08 15:55:45 +0200

    creating AddTourViewModel

--- TourPlanner/obj/Debug/net6.0-windows/TourPlanner.AssemblyInfo.cs
+++ TourPlanner/obj/Debug/net6.0-windows/TourPlanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061ac8fc794ea23cd30e2f9d592e9f81eb322d90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41af3040385189a47866e1063fcbdc30c4b8c848")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
